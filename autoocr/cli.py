2b3d1aee8e032d4008c50d5c8f37ff9d95636789
Ryan Timmons <ryan.timmons@mongodb.com>
2020-01-11 15:21:29 -0500

    python lambdas are late-binding so need functools.partial

--- autoocr/cli.py
+++ autoocr/cli.py
@@ -5,6 +5,7 @@ import threading
 import re
 import subprocess
 import filelock
+from functools import partial
 
 
 def _list_files(root_dir: str) -> typ.Set[str]:
@@ -28,7 +29,8 @@ def _need_ocr(files: typ.Set[str]) -> typ.Set[str]:
 
 
 def _run_ocr_on_file(file: str):
-    if os.path.exists(_ocred_name(file)):
+    result = _ocred_name(file)
+    if result is not None and os.path.exists(result):
         return  # defensive programming
     print(f"Starting pdfsandwich {file}")
     subprocess.run(
@@ -41,12 +43,22 @@ def _run_ocr_on_file(file: str):
 
 
 def _run_ocr_threaded(files: typ.Set[str]):
-    targets = [lambda: _run_ocr_on_file(f) for f in files]
-    threads = [threading.Thread(target=target) for target in targets]
+    threads = []
+    for f in files:
+        threads.append(threading.Thread(target=partial(_run_ocr_on_file, f)))
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
+
+
+def _cleanup(all_pdf_files: typ.Set[str]):
+    # lol let's just run _ocred_name like 1000 times
+    to_remove = [f for f in all_pdf_files
+                 if _ocred_name(f) is not None
+                 and _ocred_name(f) != f
+                 and os.path.exists(_ocred_name(f))]
+    print(f"Would remove {to_remove}")
 
 
 def main(args=None):
@@ -55,4 +67,6 @@ def main(args=None):
     with filelock.FileLock(".autoocr_running"):
         _run_ocr_threaded(_need_ocr(set(args[1:])))
         os.remove(".autoocr_running")
-
+    # poor-man's arg-parsing :)
+    if "--cleanup" in set(args[1:]):
+        _cleanup(set(args[1:]))
